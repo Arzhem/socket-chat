519e245d551e64ab31b492b53789b8276f4bc770
Arzhem <arslanhamza2007@gmail.com>
2025-06-19 13:09:52 +0300

    Change database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
     connectionStateRecovery: true
 });
 
-mongoose.connect('mongodb://localhost:27017/demo_db', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/test_demo_db', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err));
 
